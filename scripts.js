3a4330f90694fcdb77a57551ce4aa3112d994f1d
Nate Griggs <nateghsc09@gmail.com>
2021-03-13 19:04:11 -0600

    html done and added current time

---
+++ scripts.js
@@ -0,0 +1,27 @@
+var day = {
+    "8am" : "",
+    "9am" : "",
+    "10am" : "",
+    "11am" : "",
+    "12pm" : "",
+    "1pm" : "",
+    "2pm" : "",
+    "3pm" : "",
+    "4pm" : "",
+    "5pm" : "",
+    "6pm" : "",
+
+
+
+};
+
+
+
+
+
+
+
+$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+
